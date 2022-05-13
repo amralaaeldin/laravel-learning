e1eb7694a860923ea1f236f283430411d3abe6f9
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-05-13 07:13:04 +0200

    api, protect routes

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use App\Http\Requests\StorePostRequest;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
+
+class PostController extends Controller
+{
+    public function index () {
+
+        return PostResource::collection(Post::withTrashed()->paginate(15));
+
+    }
+    public function show ($post) {
+
+        return new PostResource(Post::find($post));
+    }
+    public function store (StorePostRequest $request) {
+
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+        return Post::create(array_merge($request->validated(),['slug'=>$slug]));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,43 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required','confirmed'],
+        'name' => ['required','string'],
+        'token_name' => ['required','string'],
+    ]);
+
+    /*
+    ** scenarios
+    validate data,
+    if not exist >> create it and give token
+    if exist with wrong credentials >> error
+    if exist >> give new token ,, so I didn't used unique:users validation
+    */
+
+    $user = User::where('email',  $request->email)->first();
+
+    if ( $user && ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.']
+        ]);
+    }
+    if (! $user) {
+        $user = User::create(array_merge($request->all(),['password'=>Hash::make($request->password)]));
+    }
+    $token = $user->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('posts',[PostController::class, 'index'])->name('posts.index');
+    Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('posts/store', [PostController::class, 'store'])->name('posts.store');
+    }
+);
+
+
